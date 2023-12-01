1709c6a45ea2bf26c70f54780d0c0fd71c47eb0a
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-12-01 09:56:37 -0300

    API carros || Finalizada

---
+++ src/services/CarroService.js
@@ -0,0 +1,54 @@
+const db = require('../db')
+module.exports = {
+    buscarTodos: () => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM carros', (error, results) => {
+                if (error) { rejeitado(error); return; }
+                aceito(results);
+            });
+        });
+    },
+
+    buscarUm: (codigo) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM carros WHERE codigo = ?', [codigo], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                aceito(results);
+            });
+        });
+    },
+
+    inserir: (modelo, placa) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('INSERT INTO carros(modelo, placa) VALUES(?,?)',
+                [modelo, placa],
+                (error, results) => {
+                    if (error) { rejeitado(error); return; }
+                    aceito(results.insertId);
+                });
+        });
+    },
+
+    alterar: (codigo, modelo, placa) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('UPDATE carros SET modelo=?, placa=? WHERE codigo=?', [modelo, placa, codigo], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                aceito(results.affectedRows);
+            })
+        })
+    },
+
+    excluir: (codigo) => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('DELETE FROM carros WHERE codigo = ?', [codigo], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                aceito(results.affectedRows);
+            })
+        })
+    },
+
+}
