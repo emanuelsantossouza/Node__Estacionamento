ccfe11e4f951c540f6999116737a16c8aad4bdc4
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-12-02 18:05:16 -0300

    Preparando para o deploy no vercel

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const app = express();
 
 const cors = require('cors'); // Permite acesso para autorizar o uso da API
 const bodyParser = require('body-parser'); // Permite transformar o corpo de um requisição em varios outros formatos
+const router = require('./routes');
 
-server.use(cors()); // Autoriza o uso da API com o cors
-server.use(bodyParser.urlencoded({ extended: false })); // Criando o nosso middleware body
+app.use(cors()); // Autoriza o uso da API com o cors
+app.use(bodyParser.urlencoded({ extended: false })); // Criando o nosso middleware body
+
+app.use(router); // Usando o nosso middleware router
 module.exports = app;
