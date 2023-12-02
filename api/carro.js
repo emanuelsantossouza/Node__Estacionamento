540d8b0e4f5792870a677f1f7ce2c4021ce39d62
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-12-02 18:47:59 -0300

    Preparando para o deploy no vercel

--- api/carro.js
+++ api/carro.js
@@ -1,6 +1,14 @@
 const app = require("../app");
-const route = require("../routes/carro");
 
-app.use("/api", route);
+const carro = require("express").Router();
+
+carro.get("/carro", async function (req, res) {
+    const { carroId } = req.query;
+    if (carroId) {
+        res.status(200).send({ carroName: "Some Book", carroId: carroId });
+    } else {
+        res.send("No book ID");
+    }
+});
 
 module.exports = app;
