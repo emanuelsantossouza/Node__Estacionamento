e513fe2477467dae3bfd6c23156a0a796eb52be2
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-12-02 19:11:11 -0300

    Preparando para o deploy no vercel

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bodyParser = require('body-parser');
+
+
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 module.exports = app;
