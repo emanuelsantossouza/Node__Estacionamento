daccd635bd03bd3ce1c13db7db27f1be083468d9
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-12-02 20:05:25 -0300

    Preparando para o deploy no vercel

--- routes/carros.js
+++ routes/carros.js
@@ -4,22 +4,18 @@ const carros = require("express").Router();
 const CarroController = require("../controllers/carroController");
 
 carros.get("/carros", async function (req, res) {
-
     console.log("Entrou na funcção carros router");
-    let buscaCarros = await CarroController.buscarTodos().catch((error) => {
-        console.log("Erro ao buscar carros");
+
+    try {
+        let buscaCarros = await CarroController.buscarTodos()
         console.log(error);
+        res.json(buscaCarros);
 
-    });
-    console.log(buscaCarros);
-
-    res.json(buscaCarros);
-
-    res.send([
-        { carroName: "Book Ben", carroId: "xcvrgf" },
-        { carroName: "Book Boom", carroId: "iyuitr" },
-        { carroName: "Book ABCD", carroId: "nfgvcd" },
-    ]);
+    } catch (error) {
+        console.log(error);
+        res.json({ error: error });
+        res.status(500).json({ error: 'Erro ao buscar carros' });
+    };
 });
 
 module.exports = carros;
