bf4c307e21e17f271defc7d3a853553b728e060a
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-12-02 18:00:02 -0300

    Preparando para o deploy no vercel

--- src/api/carro.js
+++ src/api/carro.js
@@ -1,6 +1,11 @@
 const app = require('./app');
-
 const server = require('../server');
+
+app.get('/carros', (req, res) => { 
+    res.send('Hello World!');
+})
+    
+
 
 module.exports = app;
 module.exports = server;
